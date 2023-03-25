416fb737c6f5025a519601f2eb1808cd790e6dbe
AB7zz <abhinavcv007@gmail.com>
2023-03-25 16:41:23 +0530

    2.12-2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,16 +3,20 @@ import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
 import axios from 'axios'
+import functions from './services/persons'
 
 function App() {
-
   const [persons, setPersons] = useState([]) 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
+    const fetchData = async() => {
+      try{
+        const res = await functions.getAll()
+        setPersons(res)
+      }catch(error){
+        console.log('Error 1: ', error)
+      }
+    }
+    fetchData()
   }, [])
   const [copy, setCopy] = useState([{}])
 
@@ -20,13 +24,30 @@ function App() {
   const [newPhone, setNewPhone] = useState('')
   const [filter, setFilter] = useState('')
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    if(persons.some(person => person.name === newName)){
-      window.alert(`${newName} is already added to phonebook`)
-      return
-    } 
-    setPersons(prev => [...prev, {name: newName, number: newPhone}])
+  const handleSubmit = async(e) => {
+    try{
+      e.preventDefault()
+      const newPerson = {
+        name: newName,
+        number: newPhone
+      }
+      if(persons.some(person => person.name === newName)){
+        const samePerson = persons.find(person => person.name == newName)
+        if(window.confirm(`${newName} is already added to phonebook, do you want to update their phone number ?`)){
+          const res = await functions.update(samePerson.id, newPerson)
+
+          setPersons(persons.map(person => person.id != samePerson.id ? person : res))
+        }else{
+          return
+        }
+      }else{
+        const res = await functions.create(newPerson)
+        
+        setPersons([...persons, res])
+      }
+    }catch(error){
+      console.log('Error 2: ', error)
+    }
   }
 
   const handleFilter = (e) => {
@@ -36,6 +57,17 @@ function App() {
     setCopy(filtered)
   }
 
+  const handleDelete = async(id, name) => {
+    try{
+      if(window.confirm(`Are you sure you want to delete ${name} ? `)){
+        const res = await functions.Delete(id)
+        setPersons(persons.filter(person => person.id != id))
+      }
+    }catch(error){
+      console.log('Error 3: ', error)
+    }
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -43,7 +75,7 @@ function App() {
       <h2>add a new</h2>
       <PersonForm setNewName={setNewName} setNewPhone={setNewPhone} handleSubmit={handleSubmit} />
       <h2>Numbers</h2>
-      <Persons filter={filter} persons={persons} copy={copy} />
+      <Persons filter={filter} persons={persons} copy={copy} handleDelete={handleDelete} />
     </div>
   )
 }
--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
-const Persons = ({filter, persons, copy}) => {
+const Persons = ({filter, persons, copy, handleDelete}) => {
   return (
     <>
     {filter == '' ? persons.map(person => 
-        <div key={person.name} style={{display: 'flex'}}>
+        <div key={person.id} style={{display: 'flex'}}>
           <p style={{marginRight: '10px'}}>{person.name}</p>
           <p>{person.number}</p>
+          <button onClick={() => handleDelete(person.id, person.name)}>delete</button>
         </div>
     ) : copy.map(person => 
-        <div key={person.name} style={{display: 'flex'}}>
+        <div key={person.id} style={{display: 'flex'}}>
           <p style={{marginRight: '10px'}}>{person.name}</p>
           <p>{person.number}</p>
+          <button onClick={() => handleDelete(person.id, person.name)}>delete</button>
         </div>
     )}
     </>
