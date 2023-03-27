9871d9d5e52fd45ea0f233002c52dbfd2e8958aa
AB7zz <abhinavcv007@gmail.com>
2023-03-27 23:03:30 +0530

    node

--- part3/index.js
+++ part3/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 
 const app = express()
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+
+app.use(cors())
 
 app.use(express.json())
 
